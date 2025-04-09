3ca1ba22959726a3870ff39ae0532958b3a9b3ff
ishardcoded <ishardcoded@gmail.com>
2025-04-09 20:27:36 +0500

    Реализовали функцию createNewElement

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ class CreateElement {
 		this.className = className
 		this.content = content
 	}
+
+	createNewElement() {
+		const newElement = document.createElement(this.tag)
+		newElement.classList.add(this.className)
+		newElement.textContent = this.content
+
+		document.body.append(newElement)
+	}
 }
+
+new CreateElement('p', 'paragraph', 'Новый текст!').createNewElement()
+
+new CreateElement('h1', 'title', 'Заголовок').createNewElement()
+
+new CreateElement('button', 'btn-primary', 'Кнопка').createNewElement()
